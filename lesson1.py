9828ed04dd268da4da4b580f6ea36f36388b12b8
Lilitgrig753 <lilitgrig753@gmail.com>
2022-04-08 21:08:04 +0400

    test

---
+++ lesson1.py
@@ -0,0 +1,342 @@
+# x=5
+# y=4
+# print(x+y)
+# input('Hello')
+# card_pin = int(input('Please enter Your pin code\n\n\n------------'))
+# print(card_pin)
+# x = 5
+# print(5)
+# popoxakan = 'hello'
+# print (popoxakan * 20)
+# x = 15
+# text ='python'
+# a = float (input('(Mutqagrek erankyan koxm 1 y----->>>.'))
+# b = float (input('Mutcareq erankyan koxm 2 '))
+# S = a*b/2
+# print(S)
+# a = float (input('Mutkagrek qarakusu moxmy 1,x---->>>'))
+# S = a**2
+# print(S)
+# harc1 = input ('Erb en stexvel tarery \n a)451 , b)erek \nc )405, td) 1951\n\t\t\t---->>>') == 'c'
+# print (harc1 == 'b')
+# a = float (input('Mutkagrek erankyan koxmy 1 a----->>>'))
+# b = float (input ('Mutkagrek erankyan koxmy 2 b----->>'))
+# S = a *b /2
+# print (S)
+# C = float(input('mutkagrel jarmastichany celsius------>>>'))
+# F = 1.8 * C + 32
+# print (F)
+# minute = float (input('mutk minute qanakay---->>'))
+# varkyan = 60 * minute
+# print(varkyan)
+# kg = float (input ('mutkagrel kg chapy------->>>'))
+# lb = kg * 0.45
+# print (lb)
+# km = float (input('muktagrel km chapy------>>>'))
+# ml = 1.609344 * km
+# print(ml)
+# text1= 'py' * 3 + 'thon'
+# print(text1)
+# text2 = '7p' * 3 + '79'
+# print(text2)
+# text2 = '9' * 7
+# print (text2)
+# balance = x + y + z
+# x, y, z = 5, 6, 7
+# print(balance)
+# import math as m
+# print (m.sqrt(16))
+# x = 'text'
+# print(type(x))
+# x = 9
+# print(type(x))
+# p = 8.5
+# print(type(p))
+# import calendar
+# print(calendar.month(1976, 9))
+# import datetime
+# x = datetime.datetime.now()
+# # print(x)
+# import random
+
+# user_number = int(input('ENTER YOUR NUMBER ----'))
+# pc_number = random.randint(1, 10)
+# print('mer tivy =', user_number, '\nkompi tivy - ', pc_number)
+# print(user_number ==pc_number)
+# import math
+# r = float(input('enter r value---->>'))
+# x = math.pi*r**2
+
+# print(x)
+# import random
+# print(random.randint(10, 100))
+# import random as r
+# print(r.choice('B' 'A' 'C'))
+
+# text = 'python'
+# tar = input('mukt arek tar....')
+# print(tar not in text)
+# text = 'python'
+# text2 = 'python'
+# print(text is not text_2)
+# bar = input ('mutk arek bar')
+# tar = input ('mutk arek tar')
+# print(tar in bar)
+# fridge = input ('input fruit .....>>')
+# print ('apple' in fridge or 'banana' in fridge or 'orange' in fridge)
+# model = input('Mukt arek heraxosi model ....>') =='iphone 12'
+# color = input('mut arek heraxosi gujny....') =='gold'
+# print(model and color)
+# Q1 = input('erb e stexcvel python\na) 2000, b)2012, \nc)1991, d)2020\n\t-------') == 'c'
+# print(Q1)
+# Q1 = input('erb e stexcvel python\na) 2000, b)2012, \nc)1991, d)2020\n\t-------') == 'c'
+# print(Q1)
+# pin = input('mutk arek dzer pin kody....') == '1234'
+# print(pin)
+# pin = int(input('enter your pin'))
+# print(pin == 1234)
+# age = float(input('mutk arek tariky......>>'))
+# print(17 <= age and age <= 25)
+# meqenaner = 'mers', 'audi', 'bmw'
+# taretver = '2020', '2022', '1999'
+# meqena = input('enter car -----')
+# taretiv = input ('enter year....')
+# print(taretiv in taretver and meqena in meqenaner)
+# import random as r
+# number = input('enter number ----')
+# x = str(r.randint(0,100))
+# print(x in number)
+# name = input('enter your name------->>')
+# tar = 'a'
+# print(tar in name)
+# import calendar
+# bday = '11,9,1976'
+# y = int(input('input the year.....>>'))
+# m = int(input('input the month.....>>'))
+# day = int(input('input the day......>>'))
+# print(bday)
+# fridge = input ('input fruit .....>>')
+# print ('apple' in fridge or 'banana' in fridge or 'orange' in fridge)
+# import random as r
+# pc = r.randint(1,10)
+# user = input('enter number....') 
+# print('pc=', pc)
+# print(str(pc) in user)
+# fridge = 'banan', 'apple', 'orange'
+# user_mirg
+# import math
+# n = int(input ('enter the number......>>' ))
+# print(math.factorial(n))
+# list1 =[1,2,3]
+# list[2] = [1,2,3]
+# list2 = append(5)
+# list2 = list1[:]
+# s = float(input('enter s tariky.....>>'))
+# if s <= 2:
+#     print(10.5 * s)
+# else:
+#     print(21  + 4 * (s-2))
+# y = int(input('enter the days of the year....>>'))
+# if y > 365:
+#    print('leap')
+# else:
+#    print('ok')
+# a = float(input())
+# b = float(input())
+# if a > b:
+#    print('a is max')
+# else:
+#    print('b is max')
+ # s = 12
+# t1 = 0.05
+# t2 = 0.055
+# v1 = s / t1
+# v2 = s / t2
+# print(v2)
+# a = float(input('enter a...>'))
+# b = float(input('enter b...>'))
+# c = float(input('enter c...>'))
+# if a > b > c:
+#    print('a is max and b greater than c')
+# elif a > b and b < c:
+#    print('a is max and b is greater than c')
+# elif a < b  and b > c:
+#    print('b is max and greater than c')
+# elif a == b == c:
+#    print('equal')
+# elif a < b < c:
+#    print('c is max and b is greater than a')
+# a = int(input('enter the number of the user----->'))
+# b = int(input('enter the number of the user------>'))
+# if a > b:
+#    print(a)
+# else:
+#    b
+# a = int(input("enter the age of a person----->>"))
+# b = int(input("enter the age of b person----->>"))
+# c = int(input("enter the age of c person----->> "))
+# if a > b > c:
+#    print("a is oldest, c is youngest")
+# elif a > b and b < c and a > c:
+#    print("a is greater and b is the youngest")
+# elif a < b and b < c:
+#    print("a is the youngest, and c is the oldest")
+# elif a < b and b > c and a < c:
+#    print("b is the greater and a is the youngest")
+# elif a == b == c:
+#    print("all are equal")
+# elif a == b and b > c:
+#    print("a and b are older and c is the youngest")
+# elif a == b and a < c:
+#    print("a and b are the youngest and the c is the oldest")
+# Sex = input("enter the sex of the employee-F or M>")
+# Age = input("enter the age of the employee Y or N>")
+# MS = input("enter the marital status....>")
+# if Sex = 'F':
+#    Print('MS is in Urban')
+# elif Sex = 'M' and Age =
+# import random as r
+# while True:
+#    pc = r.randint(1,20)
+#    if  pc < 10:
+#       print(pc)
+#    else:
+#       break
+# import time
+
+# password = 1234
+# count = 0
+# while True:
+#    password = input('enter the password...')
+#    if password == 1234:
+#       print('good')
+#       break
+#    else:
+#       print('mutkagrel noric')
+#       count += 1
+#    if count == 3:
+#       print('Duk blokum eq')
+#       time.sleep(10)
+#    elif count == 4:
+#       print('hajox')
+#       break
+# n = 1234
+# print(f'{n % 10} {n // 10 % 10} {n // 100 % 10} {n //1000}')
+
+# import random
+# u = int(input('enter the number of followers....>'))
+# pc = random.randint(0,10000000)
+# if (u - pc) / pc > 0.2:
+#    print('pc', 'u are the blogger') 
+# else: 
+#    print('pc is the blogger')
+# Sex = str((input("enter the sex of the employee-F or M....>")))
+# Age = float((input("enter the age of the employee......>")))
+# if Sex == 'F':
+#       print('emloyee can work in Urban')
+# elif Sex == 'M' and int(20 < Age <= 40):
+#      print('may work anywhere')
+# elif int(40 < Age < 60):
+#       print('may work anywhere')
+# else: 
+#       print('error')  
+# import random
+# chinga = ('S','P','R')
+# pc = random.choice(chinga)
+# u = str(input('Enter qar tuxt or mkrat as ....R,P,S>'))
+# if u == 'R'and pc == 'P':
+#    print('pc won')
+# elif u =='P' and pc == 'S':
+#      print('pc won')
+# elif u =='S' and pc =='R':
+#    print('pc won')
+# elif u =='S' and pc =='P':
+#    print('user won')
+# elif u =='R' and pc =='S':
+#    print('user won')
+# elif u =='R' and pc == 'P':
+#    print('pc won')
+# elif u =='P' and pc == 'R':
+#    print('user won')
+# elif u == 'P'and pc == 'S':
+#    print('pc won')
+# else:
+#    print('error')
+# x = int(input('enter the number .....>'))
+# n1 = 0
+# n2 = 1
+# print(n1)
+# print(n2)
+# for i in range(1, (x + 1)):
+#    res , n1, n2 = (n1 + n2), n2, (n1 + n2)
+#    print(res)
+# x = 12
+# y = 15
+# for i in range (y, (x * y) + 1):
+#    if i % x == 0 and i % y == 0:
+#       print(i)
+#       break
+# number = int(input('enter the num....>'))
+# res = 0
+# for i in str(number):
+#    res =+ int(i)
+#    print(res)
+# mylist = [1 , '2', 'barev', 4]
+# for i in mylist:
+#     print(type(i))
+# from unicodedata import digit
+# my_list =['Hp','Asus',Mac','Del','Lenovo'] 
+# if 'Mac' in my_list:
+#     print('True')
+# else:
+#     print("False")
+#     print(t)
+# t = ['a', 'b', 'c', 'd']
+# t2 = ['a', 'c', 'd']   
+# print(set(t)-set(t2))
+# numbers = input('Enter number.....')
+# numbers = numbers.split(' ')
+# for i in str(numbers):
+#     if int(i) % 2 == 0:
+#             print(i)   
+# list1 = ['a' , 'b' , 'a' , 'c' 'l' ,'b']
+# for i in list1:
+# list 2 []
+#    if i in not in list2:
+#     list1 == list1 remove i
+#     list(2) =append
+# fruits = ['banana' , 'apple' , 'cherry' , 'kiwi']
+# print(fruits[1:3])
+# print(fruits[:3])
+# print(fruits[-3:-1]) 
+# numbers = [456, 56, -456, 7.56, -2.34]
+# print(numbers[-1])
+# fruits = ['banana' , 'apple' , 'cherry' , 'kiwi']
+# fruits[1] = 'kiwi'
+# print(fruits)# mytext = ('cherry', 'fruits' , 'kiwi')
+# mytext1 = 'cheaaaarryfruaitashgg123'
+# x = mytext1.split('a')
+# print(x)
+# txt = "apple#banana#cherry#orange"
+# x = txt.split("#")
+# print(x)
+# numbers = input('Enter number--->>>')
+# numbers = numbers.split(' ')
+# for i in numbers:
+#     if int(i) % 2 == 0:
+#         print(i, end ='/')
+# dict1 = {'D': 56, 'E': 12, 'F': 69, 'C': 45, 'B': 23, 'A': 67}
+# my keys sorted
+# word = 'exercises'
+# new = {}
+# for i in word:
+#     nword = word.count(i)
+#     new.setdefault(i, nword)
+# print(new)
+
+
+
+
+
+
+
